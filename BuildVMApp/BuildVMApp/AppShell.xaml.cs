d98c0684837d618641ff6dc4a6183e6d67320bc2
Sweekriti Satpathy <swsat@microsoft.com>
2024-03-19 14:01:52 -0700

    first commit

---
+++ BuildVMApp/BuildVMApp/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using BuildVMApp.ViewModels;
+using BuildVMApp.Views;
+using Xamarin.Forms;
+
+namespace BuildVMApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
+
---
+++ BuildVMApp/BuildVMApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using BuildVMApp.Models;
+using BuildVMApp.ViewModels;
+
+namespace BuildVMApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
